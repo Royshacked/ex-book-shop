cc955d2beb20c7751851af5439994e01e572e7f8
Royshacked <roy.shacked@mail.huji.ac.il>
2024-02-03 18:48:37 +0200

    Implement render function in controller and delete table rows from HTML

--- js/bs.controller.js
+++ js/bs.controller.js
@@ -3,3 +3,25 @@
 function onInit() {
     render()
 }
+
+function getModel() {
+    return gBooks
+}
+
+function render() {
+    const books = getModel()
+    var strHTML = `<tr><th>Title</th><th>Price</th><th>Actions</th></tr>` + books.map(book => `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.price}</td>
+            <td>
+                <button class="read">Read</button>
+                <button class="update">Update</button>
+                <button class="delete">Delete</button>
+            </td>
+        </tr>
+    `).join('')
+
+    const elTable = document.querySelector('table')
+    elTable.innerHTML = strHTML
+}
