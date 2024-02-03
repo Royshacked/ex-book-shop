1b1a0afcff90b3963542734c010a8943ef651dd6
Royshacked <roy.shacked@mail.huji.ac.il>
2024-02-03 18:36:54 +0200

    Create template HTML and basic styling and js.controller with onInit() and service.js with basic model

--- js/bs.controller.js
+++ js/bs.controller.js
@@ -1,3 +1,5 @@
 'use strict'
 
-console.log('hi controller')
+function onInit() {
+    render()
+}
--- js/bs.service.js
+++ js/bs.service.js
@@ -1,3 +1,23 @@
 'use strict'
 
-console.log('hi service')
+const gBooks =  [
+    {
+        id: 'bg4J78',
+        title: 'The adventures of Lori Ipsi',
+        price: 120,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+    {
+        id: 'bg4J78',
+        title: 'World Atlas',
+        price: 300,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+    {
+        id: 'bg4J78',
+        title: 'Zorba the Greek',
+        price: 120,
+        imgUrl: 'lori-ipsi.jpg'
+    }
+]
+
