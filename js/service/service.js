f00f1262d6dc7338c2fcfed9e16d56ee9d2e3a36
Royshacked <roy.shacked@mail.huji.ac.il>
2024-02-04 09:30:05 +0200

    Implement createBook() and createBooks() which loads from storage or create gBooks and saveBooks() to storage create saveBook() erase hard coded gBooks add saveBooks() to CRUDL

--- js/service/service.js
+++ js/service/service.js
@@ -1,34 +1,18 @@
 'use strict'
 
-const gBooks = [
-    {
-        id: 'bg4J78',
-        title: 'The adventures of Lori Ipsi',
-        price: 120,
-        imgUrl: 'img/bg4j78.jpeg'
-    },
-    {
-        id: 'bg4J79',
-        title: 'World Atlas',
-        price: 300,
-        imgUrl: 'img/bg4j79.jpeg'
-    },
-    {
-        id: 'bg4J80',
-        title: 'Zorba the Greek',
-        price: 120,
-        imgUrl: 'img/bg4j80.jpeg'
-    }
-]
+var gBooks
+_createBooks()
 
 function removeBook(id) {
     const idx = gBooks.findIndex(book => book.id === id)
     gBooks.splice(idx, 1)
+    _saveBooks()
 }
 
 function updateBook(id, price) {
     const book = gBooks.find(book => book.id === id)
     book.price = price
+    _saveBooks()
 }
 
 function addBook(title, price) {
@@ -42,9 +26,35 @@ function addBook(title, price) {
             imgUrl: 'lori-ipsi.jpg'
         }
     )
+    _saveBooks()
 }
 
 function readBook(id) {
     const book = gBooks.find(book => book.id === id)
     return book
+}
+
+function _createBook(title, price, imgUrl) {
+    return {
+        id: makeId(),
+        title: title,
+        price: price,
+        imgUrl: imgUrl,
+    }
+}
+
+function _createBooks() {
+    gBooks = loadFromStorage('bookDB')
+    if (!gBooks || gBooks.length === 0) {
+        gBooks = [
+            _createBook('The adventures of Sherlock Holmes', 120, 'img/bg4j78.jpeg'),
+            _createBook('World Atlas', 300, 'img/bg4j79.jpeg'),
+            _createBook('Zorba the Greek', 120, 'img/bg4j80.jpeg')
+        ]
+        _saveBooks()
+    }
+}
+
+function _saveBooks() {
+    saveToStorage('bookDB', gBooks)
 }
