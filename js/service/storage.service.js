5c4531472480761f32dcb80ed3f59ec140184056
Royshacked <roy.shacked@mail.huji.ac.il>
2024-02-16 12:05:25 +0200

    Cosmetic changes in storage file

--- js/service/storage.service.js
+++ js/service/storage.service.js
@@ -1,11 +1,16 @@
 'use strict'
 
-function saveToStorage(key, value) { // value gets gbooks (value) as argument and turn it into str (JSON.stringify) and set in storage name 'bookDB' (key) 
+// value gets gbooks (value) as argument and turn it into str (JSON.stringify) 
+// and set in storage name 'bookDB' (key) 
+
+function saveToStorage(key, value) { 
     const valStr = JSON.stringify(value)
     localStorage.setItem(key, valStr)
 }
 
-function loadFromStorage(key) { // load storage from bookDB (key)  and return it after turn it from str to array /(JSON.parse) 
+// load storage from bookDB (key)  and return it after turn it from str to array /(JSON.parse)
+ 
+function loadFromStorage(key) { 
     const valStr = localStorage.getItem(key)
     return JSON.parse(valStr)
 }
